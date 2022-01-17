077ba136f1a25770e5c6b94217606e5e48d58e50
thibaultmaestracci <64770013+thibaultmaestracci@users.noreply.github.com>
2022-01-17 10:47:18 +0100

    initial commit
    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# theAudioToolKit
+
+A description of this package.
---
+++ Sources/theAudioToolKit/theAudioToolKit.swift
@@ -0,0 +1,6 @@
+public struct theAudioToolKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
